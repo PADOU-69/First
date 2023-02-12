2148c0f58fbaec17216b7db71a7b1fb3a24c55f4
PADOU-69 <padouGitHub.10.jcpolih@spamgourmet.com>
2023-02-12 18:34:45 +0100

    V 2.3

--- First/ViewController.swift
+++ First/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var messageLabel: UILabel!
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		messageLabel.text = "👍 C'est parti !"
 		// Do any additional setup after loading the view.
 	}
 
-
+	@IBAction func messageButtonPressed(_ sender: UIButton) {
+		messageLabel.text = "😎 Bonjour !"
+	}
+	
 }
 
